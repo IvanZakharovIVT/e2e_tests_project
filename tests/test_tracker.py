650f0035ff2cf8d9f189f379a514b1f97337ea95
ivanz <ivanzahna@gmail.com>
2025-05-15 21:05:28 +0400

    Поиск python

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -5,5 +5,9 @@ from fixtures.page import page
 
 class TestFirst:
     def test_using_fixture(self, page):
-        page.goto("google.com")
-        print("123")
+        page.goto("https://www.wikipedia.org")
+        input_field = page.locator('input[id="searchInput"]')
+        input_field.fill("Гвидо Ван Россум")
+        input_field.press("Enter")
+
+        assert "Python" in page.inner_text("body")
