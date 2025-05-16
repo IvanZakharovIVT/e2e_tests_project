7b0cbc3553374341e366451628598ac7d5b671f3
ivan <ivan.zakharov@nordclan.com>
2025-05-16 16:32:33 +0400

    Тестовый коммит с проверкой

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,4 +1,5 @@
 import time
+
 from datetime import datetime
 
 from fixtures.page import page
@@ -6,6 +7,8 @@ from settings import TRACKER_USERNAME, TRACKER_PASSWORD
 
 
 class TestTracker:
+    # comment_text = "Разработка первых автотестов для работы с playwright"
+    comment_text = "playwright"
     def test_set_and_remove_time(self, page):
         page.goto("http://track.nordclan/timereports")
         input_username = page.locator('input[name="username"]')
@@ -32,20 +35,28 @@ class TestTracker:
         add_button = page.locator('button[type="submit"]')
         add_button.click()
 
+        toggles = page.locator('.toggleComment')
+        toggle_count = len(toggles.all())
+
         first_untrack_day = page.locator('input[value="0"]').first
         first_untrack_day.click()
         first_untrack_day.fill("8")
 
-        time.sleep(5)
-
-        comment = page.locator('.toggleComment').last
-        comment.click()
+        comment = page.locator(f'tr.taskRow > td:nth-child({toggle_count + 2}) > div > div > .toggleComment')
+        comment.click(timeout=5000)
 
         comment_input = page.locator('textarea')
+        comment_input.click()
         comment_input.fill("Комментарий")
 
         confirm_button = page.locator('.XRVp4xxKofwvTTn6y8Y2')
         confirm_button.click()
+
+
+        date_to_check = page.locator(
+            f'.GSJEaIhqhOj5a1bwaWXu > th:nth-child({toggle_count + 2}) > div'
+        ).text_content()[2:]
+        time.sleep(2)
 
         total_toggle_comment = page.locator('.totalToggleComment')
         total_toggle_comment.click()
@@ -53,9 +64,12 @@ class TestTracker:
         toggle_list_locator = page.locator('.rGOECBNcTT2M4YU4zlJX')
         last_element = toggle_list_locator.locator('.SBRJyKo57H5f0mT3YNNL').last
 
-        result_value = last_element.locator('textarea').last.input_value()
+        textarea = last_element.locator('textarea')
+        textarea.wait_for(timeout=2000, state="attached")
+
+        result_value = textarea.input_value()
         assert result_value == "Комментарий"
 
-        time_value = last_element.locator('.FHz42jZNAtn160bzlTQK').text_content(timeout=10)
-        assert time_value.split(' ')[1] == datetime.today().date().strftime('%d.%m')
+        time_value = last_element.locator('.FHz42jZNAtn160bzlTQK').text_content()
+        assert time_value.split(' ')[1] == date_to_check
 
